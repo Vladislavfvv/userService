05c99a5d84a2ad0f6558a71d778043cb6f14b092
Vlad <vladfilimontsev@yandex.ru>
2025-11-03 14:35:08 +0300

    Fix Tests - create property for TestConteiner

--- src/test/java/com/innowise/demo/DemoApplicationTests.java
+++ src/test/java/com/innowise/demo/DemoApplicationTests.java
@@ -2,9 +2,34 @@ package com.innowise.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+//@Disabled("Temporary")
+@Testcontainers
 @SpringBootTest
 class DemoApplicationTests {
+
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.liquibase.enabled", () -> "false");// Отключаем Liquibase для тестов
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.cache.type", () -> "redis");
+        registry.add("spring.cache.redis.time-to-live", () -> "600000");
+    }
 
     @Test
     void contextLoads() {
